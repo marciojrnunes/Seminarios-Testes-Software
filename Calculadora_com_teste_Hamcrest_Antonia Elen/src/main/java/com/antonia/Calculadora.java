56f80e11f71e988f83bbe0cb4c248aff3520dd74
elenchaves <elemchaves@hotmail.com>
2025-05-04 19:44:29 -0300

    Foi adicionado a pasta com o arquivo do código usado na apresentação sobre o teste de software com hamcrest.

---
+++ Calculadora_com_teste_Hamcrest_Antonia Elen/src/main/java/com/antonia/Calculadora.java
@@ -0,0 +1,21 @@
+package com.antonia;
+
+public class Calculadora {
+
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public int dividir(int a, int b) {
+        if (b == 0) throw new ArithmeticException("Divisão por zero");
+        return a / b;
+    }
+}
